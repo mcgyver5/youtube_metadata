9600880dd417dc4d3f249a9f90403b08ecae97ee
mcgyver5 <tim.e.mcguire@gmail.com>
2020-03-29 22:06:48 +0000

    regex to convert runtime

--- test_metadata.py
+++ test_metadata.py
@@ -1,13 +1,18 @@
 import unittest
+import metadata
 
 
 class TestMetadata(unittest.TestCase):
 
     def test_ConvertDate(self):
-        expected = '12:06'
-        actual = '12:16'
+        expected = '39:13'
+        actual = metadata.convertDate('PT39M13S')
         self.assertEqual(expected, actual)
-
+    
+    def test_ConvertDate2912(self):
+        expected = '29:12'
+        actual = metadata.convertDate('PT29M12S')
+        self.assertEqual(expected, actual)
 if __name__ == '__main__':
     unittest.main()
 
